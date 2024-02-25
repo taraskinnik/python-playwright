8a5bdff5eb380305bf7c1cae01997aa057b99bb4
taraskinnik <62111315+taraskinnik@users.noreply.github.com>
2024-02-25 12:57:12 +0200

    Create login.py

---
+++ pages/wazzup/login.py
@@ -0,0 +1,29 @@
+from playwright.sync_api._generated import ElementHandle
+
+from pages.base import Base
+
+
+class Login(Base):
+    @property
+    def password_field(self) -> ElementHandle:
+        return self.userform.wait_for_selector("#password")
+
+    @property
+    def submit_button(self) -> ElementHandle:
+        return self.userform.wait_for_selector("#submit")
+
+    @property
+    def username_field(self) -> ElementHandle:
+        return self.userform.wait_for_selector("#text")
+
+    def fill_form(self, user: dict) -> None:
+        """Fill out the login form.
+
+        :param user: A user intended for login.
+        """
+        self.username_field.fill(user["userName"])
+        self.password_field.fill(user["password"])
+
+    def navigate(self) -> None:
+        """Navigate to the login page."""
+        self.page.goto(f"{self.base_url}/login")
