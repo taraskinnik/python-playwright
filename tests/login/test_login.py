2b137f42132739ec7da6eb87905deaede09a26ed
taraskinnik <62111315+taraskinnik@users.noreply.github.com>
2024-02-25 14:06:09 +0200

    Update test_login.py

--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -1,7 +1,6 @@
 import pytest
 from playwright.sync_api import Page
 
-from credentials import user
 from pages import Login, Channels
 
 
@@ -18,14 +17,7 @@ class TestLogin:
         channels_page = Channels(page)
 
         login_page.navigate()
-        login_page.fill_form(user)
+        login_page.fill_form({"userName": "test_user", "password": "test_password"})
         login_page.submit_button.click()
 
         expect(channels_page.username_value_field).to_have_text("Wazzup: 9496-6880")
-
-        with page.expect_response("**/BookStore/v1/Books") as response:
-            books_page.navigate()
-
-        visible: bool = books_page.book(title=book_title).is_visible()
-
-        assert visible and response.value.ok
