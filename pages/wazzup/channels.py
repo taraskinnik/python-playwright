eb523b45e9fd6a94c9086f98f3c0d95f7a25bded
taraskinnik <62111315+taraskinnik@users.noreply.github.com>
2024-02-25 13:37:00 +0200

    Update channels.py

--- pages/wazzup/channels.py
+++ pages/wazzup/channels.py
@@ -3,7 +3,7 @@ from playwright.sync_api._generated import Locator
 from pages.private import PrivatePage
 
 
-class Profile(PrivatePage):
+class Channels(PrivatePage):
     @property
     def username_value_field(self) -> Locator:
         # self.menu_container.locator(".drawer-header-title")
