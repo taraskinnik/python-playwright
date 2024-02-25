8d284bdca9cdba919c341c1b56aa8c7b5de78da1
taraskinnik <62111315+taraskinnik@users.noreply.github.com>
2024-02-25 14:56:16 +0200

    Update private.py

--- pages/private.py
+++ pages/private.py
@@ -6,5 +6,4 @@ from pages.base import Base
 class PrivatePage(Base):
     @property
     def menu_container(self) -> Locator:
-        # return self.page.locator(".drawer-menu-container")
-        return self.page.get_by_role("drawer-menu-container")
+        return self.page.locator(".drawer-menu-container")
