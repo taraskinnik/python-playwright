7bed2609049870da901ab0c13bc8e7e30fb993e5
taraskinnik <62111315+taraskinnik@users.noreply.github.com>
2024-02-25 14:57:30 +0200

    Update channels.py

--- pages/wazzup/channels.py
+++ pages/wazzup/channels.py
@@ -6,8 +6,7 @@ from pages.private import PrivatePage
 class Channels(PrivatePage):
     @property
     def username_value_field(self) -> Locator:
-        # self.menu_container.locator(".drawer-header-title")
-        return self.menu_container.get_by_role("drawer-header-title")
+        return self.menu_container.locator(".drawer-header-title")
 
     def navigate(self) -> None:
         self.page.goto(f"{self.base_url}/settings/channels")
